15de0f0b8aa2ef4f98285e00849930b0a79ca42a
Artem Bulgakov <artyombulgakov@gmail.com>
2022-06-04 23:44:12 +0300

    refactor

--- util/tsparser.py
+++ util/tsparser.py
@@ -2,14 +2,10 @@
 
 from __future__ import annotations
 
-import io
 from typing import List, Tuple, Callable
 import os
 from dataclasses import dataclass, field
-import util
-import enum
-
-PROC_NUMBER = 2
+import config
 
 @dataclass
 class Task:
@@ -22,7 +18,7 @@ class Task:
 
 @dataclass
 class TaskSystem:
-    m: int = field(default=PROC_NUMBER)
+    m: int = field(default=config.PROC_NUM)
     n: int = field(default=0)
     n_heavy: int = field(default=0)
     util: float = field(default=0)
@@ -34,103 +30,6 @@ class TaskSystem:
         return self.n, self.n_heavy, self.util, self.d_ratio, self.pd_ratio
 
 
-@enum.unique
-class VaryingParameters(enum.IntEnum):
-    N = 0
-    N_heavy = 1
-    Utilization = 2
-    D_ratio = 3
-
-    def __str__(self):
-        return self.name
-
-
-@dataclass
-class TestSet:
-    """
-        Mostly a container for a collection of TaskSystems.
-        Automatically detects varying quantity for further use (in comparisions and plotting)
-    """
-    tasksys_list: List[TaskSystem] = field(default_factory=list)
-    varying_param: VaryingParameters = field(default=None)
-    input_file_name: str = field(default=None)
-
-    def __init__(self, tasksys_list: List[TaskSystem], input_file_name: str):
-        self.tasksys_list = tasksys_list
-        self.input_file_name = input_file_name
-        if len(tasksys_list) > 0:
-            self.detect_varying_parameter()
-        if self.varying_param is not None:
-            util.print_if_interactive(f'Autodetect varying parameter: {str(self.varying_param)}', __name__)
-        else:
-            raise ValueError('Failed to autodetect varying parameter')
-
-    def get_total_tasksets_num(self) -> int:
-        ans = 0
-        for tasksys in self.tasksys_list:
-            ans += len(tasksys.taskset)
-        return ans
-
-    def get_varying_parameters(self) -> List:
-        return [tasksys.sysparam_tuple()[int(self.varying_param)] for tasksys in self.tasksys_list]
-
-    def detect_varying_parameter(self) -> VaryingParameters:
-        if self.varying_param is None:
-            prev_tsys = self.tasksys_list[0]
-            for tsys in self.tasksys_list[1:]:
-                tup = prev_tsys.sysparam_tuple()
-                prev_tup = tsys.sysparam_tuple()
-                if tup != prev_tup:
-                    if tsys.n != prev_tsys.n:
-                        self.varying_param = VaryingParameters.N
-                    elif tsys.n_heavy != prev_tsys.n_heavy:
-                        self.varying_param = VaryingParameters.N_heavy
-                    elif tsys.util != prev_tsys.util:
-                        self.varying_param = VaryingParameters.Utilization
-                    elif tsys.d_ratio != prev_tsys.d_ratio:
-                        self.varying_param = VaryingParameters.D_ratio
-                    else:
-                        assert False
-        return self.varying_param
-
-'''
-def parse_taskset_file(input_file_name: str) -> TestSet:
-    """
-    Parses input file (CPLEX-generated tasksets) into a collection of TaskSystem representation (a TestSet).
-    :param input_file_name:
-    :return:
-    """
-    testset_arr = []
-    tasksys_dict = dict()
-    with open(os.path.abspath(input_file_name), 'r') as inp_file:
-        while True:
-            in_str = inp_file.readline()
-            if len(in_str) <= 0:
-                break
-            elif in_str[0] == '#':
-                continue
-            vals = []
-            # parse numerical tab-delimited values into a list of ints (and floats)
-            for v in in_str.split('\t'):
-                try:
-                    vals.append(int(v.strip()))
-                except ValueError:
-                    vals.append(float(v.strip()))
-            sysparams_tup = tuple(vals[0:5])
-            if sysparams_tup not in tasksys_dict:
-                tasksys_dict[sysparams_tup] = TaskSystem(PROC_NUMBER, *sysparams_tup)
-            cur_tasksys = tasksys_dict[sysparams_tup]  # get system parameters
-            # cut to tasks description only
-            vals = vals[5:]
-            taskset = []
-            while len(vals) > 0:
-                task = Task(*vals[0:3])
-                vals = vals[3:]
-                taskset.append(task)
-            assert len(taskset) == cur_tasksys.n
-            cur_tasksys.taskset.append(taskset)
-    return TestSet(list(tasksys_dict.values()), input_file_name)
-'''
 
 
 def read_and_evaluate(input_file_name: str, unit_action: Callable, action_args: List):
@@ -147,7 +46,7 @@ def read_and_evaluate(input_file_name: str, unit_action: Callable, action_args: 
         while True:
             in_str = inp_file.readline()
             lineno += 1
-            print(f'Test {lineno}/{total_lines}')
+            print(f'Test {lineno}/{total_lines}...', end=' ')
             if len(in_str) <= 0:
                 break
             elif in_str[0] == '#':
@@ -160,7 +59,7 @@ def read_and_evaluate(input_file_name: str, unit_action: Callable, action_args: 
                 except ValueError:
                     vals.append(float(v.strip()))
             sysparams_tup = tuple(vals[0:5])
-            cur_tasksys = TaskSystem(PROC_NUMBER, *sysparams_tup)
+            cur_tasksys = TaskSystem(config.PROC_NUM, *sysparams_tup)
             # cut to tasks description only
             vals = vals[5:]
             while len(vals) > 0:
